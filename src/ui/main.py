d3f8ba769687fb5b681638506e1ee67570255b29
Hassaku-kun <masaki.tani526@gmail.com>
2018-10-27 23:16:13 +0900

    :tada: GUIの画面を追加

---
+++ src/ui/main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox as tkMessageBox
+from tkinter import filedialog as tkFileDialog
+import os
+
+def play():
+    print('Play %s' % filename.get())
+
+def pick():
+    print('pick a file')
+    fTyp = [('音声ファイル', '*.wav')]
+    iDir = '/home/Documents'
+    filename.set(tkFileDialog.askopenfilename(filetypes = fTyp, initialdir = iDir))
+    print(filename.get())
+
+root = Tk()
+root.title('Brain Melts')
+frame1 = ttk.Frame(root)
+frame2 = ttk.Frame(frame1)
+
+filename = StringVar()
+
+logo = PhotoImage(file = 'assets/logo.png')
+canvas1 = Canvas(frame1, width=500, height=500, bg='#15738c')
+canvas1.create_image(250, 250, image=logo)
+entry1 = ttk.Entry(frame2, textvariable=filename)
+button1 = ttk.Button(frame2, text='pick a wav file', command=pick)
+button2 = ttk.Button(frame2, text='play', command=play)
+
+frame1.grid(row=0, column=0, sticky=(N,E,S,W))
+canvas1.grid(row=1, column=1, sticky=E)
+frame2.grid(row=1, column=2, sticky=W)
+entry1.grid(row=1, column=1, sticky=E)
+button1.grid(row=1, column=2, sticky=W)
+button2.grid(row=2, column=1, sticky=S)
+
+for child in frame1.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+root.mainloop()
