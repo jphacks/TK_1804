4e2f383c8fa55cfba8b5b037decce4b43da039f7
Masamune Kobayashi <sp.16.sy+git@gmail.com>
2018-10-28 14:31:58 +0900

    :+1: GUIからmp3を選択できるようにした

--- src/gui.py
+++ src/gui.py
@@ -11,8 +11,8 @@ def play():
     print('Play %s' % filename.get())
 
 def pick():
-    print('pick a file')
-    fTyp = [('音声ファイル', '*.wav')]
+    print('pick a audio file')
+    fTyp = [('wavファイル', '*.wav'), ('mp3ファイル', '*.mp3')]
     iDir = '/home/Documents'
     filename.set(tkFileDialog.askopenfilename(filetypes = fTyp, initialdir = iDir))
     print(filename.get())
@@ -28,7 +28,7 @@ logo = PhotoImage(file = './src/assets/logo.gif')
 canvas1 = Canvas(frame1, width=500, height=500, bg='#15738c')
 canvas1.create_image(250, 250, image=logo)
 entry1 = ttk.Entry(frame2, textvariable=filename)
-button1 = ttk.Button(frame2, text='pick a wav file', command=pick)
+button1 = ttk.Button(frame2, text='pick a audio file', command=pick)
 button2 = ttk.Button(frame2, text='play', command=play)
 
 frame1.grid(row=0, column=0, sticky=(N,E,S,W))
